4e6d56b10140c1aeded0cea06a454c586a1909b3
davidmun <davidmun@microsoft.com>
2015-12-07 15:46:18 -0800

    vsvim uses the right clipboard.

--- setup.cmd
+++ setup.cmd
@@ -1,6 +1,6 @@
 setlocal
-set home=%homedrive%%homepath%\
-REM git really doesn't like Windows symlinks
+set home=%userprofile%\
+REM git doesn't like symlinks
 mklink /H %home%.gitconfig %~dp0.gitconfig
-mklink %home%.vimrc %~dp0.vimrc
-mklink %home%.vsvimrc %~dp0.vsvimrc
+mklink /H %home%.vimrc %~dp0.vimrc
+mklink /H %home%.vsvimrc %~dp0.vsvimrc
